5e9a743a1de5f31378335218548d0c3eedb784d2
= <=>
2014-04-12 21:56:13 -0400

    fixed the css and improved the js to handle cases where some text boxes were not filled

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,51 @@
 function calcGrade() {
-	var grade1, grade2, grade3, grade4, grade5, grade6;
+	//var grade1, grade2, grade3, grade4, grade5, grade6;
 	var totalPercent = 0;
 	var sum = 0;
+	var numGrades = 0;
+	var percents = new Array();
+	var grades = new Array();
+	var numGrades = 4;
 
-	totalPercent += parseFloat(document.getElementById('grade1PercentInputId').value);
-	totalPercent += parseFloat(document.getElementById('grade2PercentInputId').value);
-	totalPercent += parseFloat(document.getElementById('grade3PercentInputId').value);
-	totalPercent += parseFloat(document.getElementById('grade4PercentInputId').value);
-	totalPercent += parseFloat(document.getElementById('grade5PercentInputId').value);
-	totalPercent += parseFloat(document.getElementById('grade6PercentInputId').value);
+	for (var i = 1; i < 6; i++) {
+		var id = 'class'+i+'PercentId';
+		try {
+			var percent = parseFloat(document.getElementById(id).value);
+			
+			if (!isNaN(percent)) {
+				//alert('percent: '+percent);
+				totalPercent += percent;
+			}
+		}
+		catch(err) {}
+
+	}
+	//alert('percent total: '+totalPercent);
 	
 	var finalPercent = 100.0 - totalPercent;
+	for (var i = 1; i < numGrades+1; i++) {
+		try {
+			var grade = parseFloat(document.getElementById('class'+i+'GradeId').value) *
+			(parseFloat((document.getElementById('class'+i+'PercentId').value)/100));
 
-	grade1 = parseFloat(document.getElementById('grade1InputId').value) *
-		     (parseFloat((document.getElementById('grade1PercentInputId').value)/100));
+			if (!isNaN(grade)) {
+				//alert('grade:'+grade);
+				grades.push(grade);
+			}
+		}
+		catch(err) {}
+		
+	}
 
-    //alert(grade1);
-	
-	grade2 = parseFloat(document.getElementById('grade2InputId').value) *
-		     (parseFloat((document.getElementById('grade2PercentInputId').value)/100));
-	
-	grade3 = parseFloat(document.getElementById('grade3InputId').value) *
-		     (parseFloat((document.getElementById('grade3PercentInputId').value)/100));
-	
-	grade4 = parseFloat(document.getElementById('grade4InputId').value) *
-		     (parseFloat((document.getElementById('grade4PercentInputId').value)/100));
-	
-	grade5 = parseFloat(document.getElementById('grade5InputId').value) *
-		     (parseFloat((document.getElementById('grade5PercentInputId').value)/100));
-	
-	grade6 = parseFloat(document.getElementById('grade6InputId').value) *
-		     (parseFloat((document.getElementById('grade6PercentInputId').value)/100));
-	
-	sum = grade1 + grade2 + grade3 + grade4 + grade5 + grade6;
+
+	while(grades.length > 0) {
+		var grade = grades.pop();
+		alert('grade: '+grade); 
+		sum += grade;
+	}
+	//sum = grade1 + grade2 + grade3;// + grade4 + grade5 + grade6;
+	alert('sum: '+sum);
+	alert('final percent: '+finalPercent);
 	
 	var A = (90 - sum) / (finalPercent/100.0)
 	var B = (80 - sum) / (finalPercent/100.0)
@@ -42,45 +55,45 @@ function calcGrade() {
 
 	if (A > 100 || A < 0) 
 		document.getElementById('aGradeId').innerHTML = "A) <strike>"+A.toFixed(1)+"</strike>";
-	else
-		document.getElementById('aGradeId').innerHTML = "A) "+A.toFixed(1);
-	if (B > 100 || B < 0) 
-		document.getElementById('bGradeId').innerHTML = "B) <strike>"+B.toFixed(1)+"</strike>";
-	else
-		document.getElementById('bGradeId').innerHTML = "B) "+B.toFixed(1);
-	if (C > 100 || C < 0) 
-		document.getElementById('cGradeId').innerHTML = "C) <strike>"+C.toFixed(1)+"</strike>";
-	else
-		document.getElementById('cGradeId').innerHTML = "C) "+C.toFixed(1);
-	if (D > 100 || D < 0) 
-		document.getElementById('dGradeId').innerHTML = "D) <strike>"+D.toFixed(1)+"</strike>";
-	else
-		document.getElementById('dGradeId').innerHTML = "D) "+D.toFixed(1);
-	if (F > 100 || F < 0) 
-		document.getElementById('fGradeId').innerHTML = "F) <strike>"+F.toFixed(1)+"</strike>";
-	else
-		document.getElementById('fGradeId').innerHTML = "F) "+F.toFixed(1);
+else
+	document.getElementById('aGradeId').innerHTML = "A) "+A.toFixed(1);
+if (B > 100 || B < 0) 
+	document.getElementById('bGradeId').innerHTML = "B) <strike>"+B.toFixed(1)+"</strike>";
+else
+	document.getElementById('bGradeId').innerHTML = "B) "+B.toFixed(1);
+if (C > 100 || C < 0) 
+	document.getElementById('cGradeId').innerHTML = "C) <strike>"+C.toFixed(1)+"</strike>";
+else
+	document.getElementById('cGradeId').innerHTML = "C) "+C.toFixed(1);
+if (D > 100 || D < 0) 
+	document.getElementById('dGradeId').innerHTML = "D) <strike>"+D.toFixed(1)+"</strike>";
+else
+	document.getElementById('dGradeId').innerHTML = "D) "+D.toFixed(1);
+if (F > 100 || F < 0) 
+	document.getElementById('fGradeId').innerHTML = "F) <strike>"+F.toFixed(1)+"</strike>";
+else
+	document.getElementById('fGradeId').innerHTML = "F) "+F.toFixed(1);
 }
 
 function initialize() {
 	document.getElementById('class1NameId').value = "HW";
 	document.getElementById('class2NameId').value = "Quizzes";
-	document.getElementById('class3NameId').value = "Midterm";
-	document.getElementById('class4NameId').value = "Tests";
-	document.getElementById('class5NameId').value = "Projects";
-	document.getElementById('class6NameId').value = "Participation";
+	document.getElementById('class3NameId').value = "Tests";
+	//document.getElementById('class4NameId').value = "Midterm";
+	//document.getElementById('class5NameId').value = "Projects";
+	//document.getElementById('class6NameId').value = "Participation";
 
-	document.getElementById('grade1PercentInputId').value = 5;
-	document.getElementById('grade2PercentInputId').value = 15;
-	document.getElementById('grade3PercentInputId').value = 20;
-	document.getElementById('grade4PercentInputId').value = 20;
-	document.getElementById('grade5PercentInputId').value = 20;
-	document.getElementById('grade6PercentInputId').value = 5;
+	document.getElementById('class1PercentId').value = 5;
+	document.getElementById('class2PercentId').value = 15;
+	document.getElementById('class3PercentId').value = 20;
+	//document.getElementById('grade4PercentInputId').value = 20;
+	//document.getElementById('grade5PercentInputId').value = 20;
+	//document.getElementById('grade6PercentInputId').value = 5;
 
-	document.getElementById('grade1InputId').value = 90;
-	document.getElementById('grade2InputId').value = 85;
-	document.getElementById('grade3InputId').value = 90;
-	document.getElementById('grade4InputId').value = 83;
-	document.getElementById('grade5InputId').value = 85;
-	document.getElementById('grade6InputId').value = 70;
+	document.getElementById('class1GradeId').value = 90;
+	document.getElementById('class2GradeId').value = 85;
+	document.getElementById('class3GradeId').value = 90;
+	//document.getElementById('grade4InputId').value = 83;
+	//document.getElementById('grade5InputId').value = 85;
+	//document.getElementById('grade6InputId').value = 70;
 }
