e0b3cc1b14fe359a8fcc1c18c6bbb6399cf0e015
= <=>
2014-04-13 20:40:24 -0400

    added a clear data button.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function calcGrade() {
 	var percents = new Array();
 	var grades = new Array();
 	var numGrades = 4;
+	var hasGrade = false;
 
 	for (var i = 1; i < 6; i++) {
 		
@@ -15,6 +16,7 @@ function calcGrade() {
 			
 			if (!isNaN(percent)) {
 				totalPercent += percent;
+				hasGrade = true;
 			}
 		}
 		catch(err) {}
@@ -40,42 +42,47 @@ function calcGrade() {
 		sum += grade;
 	}
 	
-	var A = (90 - sum) / (finalPercent/100.0);
-	var B = (80 - sum) / (finalPercent/100.0);
-	var C = (70 - sum) / (finalPercent/100.0);
-	var D = (60 - sum) / (finalPercent/100.0);
-	var F = (50 - sum) / (finalPercent/100.0);
+	if (hasGrade) {
+		var A = (90 - sum) / (finalPercent/100.0);
+		var B = (80 - sum) / (finalPercent/100.0);
+		var C = (70 - sum) / (finalPercent/100.0);
+		var D = (60 - sum) / (finalPercent/100.0);
+		var F = (50 - sum) / (finalPercent/100.0);
+	}
+	else {
+		clearData();
+	}
 
 	if (A > 100 || A < 0) {
-		document.getElementById('aGradeId').innerHTML = "A) <strike>"+A.toFixed(1)+"</strike>";
-	}
-	else {
-		document.getElementById('aGradeId').innerHTML = "A) "+A.toFixed(1);
-	}
-	if (B > 100 || B < 0)  {
-		document.getElementById('bGradeId').innerHTML = "B) <strike>"+B.toFixed(1)+"</strike>";
-	}
-	else {
-		document.getElementById('bGradeId').innerHTML = "B) "+B.toFixed(1);
-	}
-	if (C > 100 || C < 0) {
-		document.getElementById('cGradeId').innerHTML = "C) <strike>"+C.toFixed(1)+"</strike>";
-	}
-	else {
-		document.getElementById('cGradeId').innerHTML = "C) "+C.toFixed(1);
-	}
-	if (D > 100 || D < 0)  {
-		document.getElementById('dGradeId').innerHTML = "D) <strike>"+D.toFixed(1)+"</strike>";
-	}
-	else {
-		document.getElementById('dGradeId').innerHTML = "D) "+D.toFixed(1);
-	}
-	if (F > 100 || F < 0) {
-		document.getElementById('fGradeId').innerHTML = "F) <strike>"+F.toFixed(1)+"</strike>";
-	}
-	else {
-		document.getElementById('fGradeId').innerHTML = "F) "+F.toFixed(1);
-	}
+		document.getElementById('Grade1Id').innerHTML = "A) <strike>"+A.toFixed(1)+"</strike>";
+}
+else {
+	document.getElementById('Grade1Id').innerHTML = "A) "+A.toFixed(1);
+}
+if (B > 100 || B < 0)  {
+	document.getElementById('Grade2Id').innerHTML = "B) <strike>"+B.toFixed(1)+"</strike>";
+}
+else {
+	document.getElementById('Grade2Id').innerHTML = "B) "+B.toFixed(1);
+}
+if (C > 100 || C < 0) {
+	document.getElementById('Grade3Id').innerHTML = "C) <strike>"+C.toFixed(1)+"</strike>";
+}
+else {
+	document.getElementById('Grade3Id').innerHTML = "C) "+C.toFixed(1);
+}
+if (D > 100 || D < 0)  {
+	document.getElementById('Grade4Id').innerHTML = "D) <strike>"+D.toFixed(1)+"</strike>";
+}
+else {
+	document.getElementById('Grade4Id').innerHTML = "D) "+D.toFixed(1);
+}
+if (F > 100 || F < 0) {
+	document.getElementById('Grade5Id').innerHTML = "F) <strike>"+F.toFixed(1)+"</strike>";
+}
+else {
+	document.getElementById('Grade5Id').innerHTML = "F) "+F.toFixed(1);
+}
 }
 
 function initialize() {
@@ -90,4 +97,28 @@ function initialize() {
 	document.getElementById('class1GradeId').value = 90;
 	document.getElementById('class2GradeId').value = 85;
 	document.getElementById('class3GradeId').value = 90;
+}
+
+function clearData() {
+	for (var i = 1; i < 6; i++) {
+		try {
+			var nameId = 'class'+i+'NameId';
+			var percentId = 'class'+i+'PercentId';
+			var gradeId = 'class'+i+'GradeId';
+
+			document.getElementById(nameId).value = null;
+			document.getElementById(percentId).value = null;
+			document.getElementById(gradeId).value = null;
+		}
+		catch(err) {}
+	}
+	for (var i = 1; i < 6; i++) {
+		var letter = 'A) ';
+		if(i == 2) {letter = 'B) ';}
+		else if(i == 3) {letter = 'C) ';}
+		else if(i == 4) {letter = 'D) ';}
+		else if(i == 5) {letter = 'F) ';}
+		var el = document.getElementById('Grade'+i+'Id').innerHTML = letter;
+
+	}
 }
